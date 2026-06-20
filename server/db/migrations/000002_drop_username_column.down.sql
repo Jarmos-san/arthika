3672c0e3204846aff910a33290a832d9a5da7c83
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-06-20 15:53:25 +0530

    chore: fix broken db migration scripts

--- server/db/migrations/000002_drop_username_column.down.sql
+++ server/db/migrations/000002_drop_username_column.down.sql
@@ -1,8 +1,13 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users_old (
     id           TEXT NOT NULL PRIMARY KEY,
     username     TEXT NOT NULL UNIQUE,
     email        TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
+
+INSERT INTO users_old (id, username, email, password_hash)
+SELECT id, '', email, password_hash FROM users;
+
+DROP TABLE users;
+
+ALTER TABLE users_old RENAME TO users;
