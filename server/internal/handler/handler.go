6414dd46e943798ecfd515a84ee2a36f46a08271
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-06-20 15:40:17 +0530

    chore: embed `Querier` into `Handler`

--- server/internal/handler/handler.go
+++ server/internal/handler/handler.go
@@ -4,16 +4,21 @@ import (
 	"log/slog"
 
 	"github.com/Jarmos-san/arthika/server/internal/api"
+	"github.com/Jarmos-san/arthika/server/internal/repository"
 )
 
 var _ api.StrictServerInterface = (*Handler)(nil)
 
 // Handler serves HTTP requests for the API.
 type Handler struct {
-	logger *slog.Logger
+	logger  *slog.Logger
+	querier repository.Querier
 }
 
 // NewHandler creates a new Handler.
-func NewHandler(logger *slog.Logger) *Handler {
-	return &Handler{logger: logger}
+func NewHandler(logger *slog.Logger, querier repository.Querier) *Handler {
+	return &Handler{
+		logger:  logger,
+		querier: querier,
+	}
 }
