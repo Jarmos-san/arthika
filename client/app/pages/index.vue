69930f5952a8708ba857f2ec98832c1e4b42324d
Somraj Saha <31373860+jarmos-san@users.noreply.github.com>
2026-06-23 16:09:04 +0530

    chore(client): setup the client-side application (#34)
    ## Description
    
    This PR provides the bare minimum configurations and file to set up the
    client-side application using Nuxt.js. In addition to the Nuxt.js
    frameork, the app also depends on NuxtUI for the component library
    requirements.
    
    ## Modification
    
    The list of changes introduced with the PR are as follows:
    
    1. Added a sample "Hello World!" Vue code to check if the setup was
    complete and working as expected.
    2. Configured the Nuxt.js framework to build the client-side
    application.
    3. Updated the `.gitignore` and `.editorconfig` file.

---
+++ client/app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <h1 class="text-4xl font-bold">Hello World</h1>
+  </div>
+</template>
---
+++ client/nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2026-06-23",
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: ["@nuxt/ui"],
+  ssr: false,
+});
