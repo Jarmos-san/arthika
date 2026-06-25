52df48193f5770288b87e4ddcc7395862e8daff4
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-06-25 14:25:38 +0530

    chore: update the configurations

--- client/oxlint.config.ts
+++ client/oxlint.config.ts
@@ -1,14 +1,34 @@
 import { defineConfig } from "oxlint";
 
 export default defineConfig({
-  plugins: ["typescript", "unicorn", "oxc", "vue", "jsdoc", "vitest", "eslint"],
-  categories: { correctness: "error" },
+  plugins: [
+    "typescript",
+    "unicorn",
+    "oxc",
+    "vue",
+    "jsdoc",
+    "vitest",
+    "eslint",
+    "import",
+  ],
+  categories: {
+    correctness: "error",
+  },
   rules: {},
   env: {
+    browser: true,
     builtin: true,
+    vitest: true,
+    vue: true,
   },
   options: {
     typeAware: true,
     typeCheck: true,
+  },
+  settings: {
+    jsdoc: {
+      augmentsExtendsReplacesDocs: true,
+      exemptDestructuredRootsFromChecks: true,
+    },
   },
 });
