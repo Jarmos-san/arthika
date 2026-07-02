77e230f5384b9243b571884e987e59fee7ede017
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-07-02 17:14:51 +0530

    chore: generate the TypeScript types

---
+++ client/generated/index.ts
@@ -0,0 +1,35 @@
+export type { ErrorResponse } from "./models/ErrorResponse.ts";
+export type {
+  LoginData,
+  LoginRequestConfig,
+  LoginResponses,
+  LoginStatus200,
+  LoginStatus401,
+  LoginStatus422,
+} from "./models/Login.ts";
+export type { LoginRequest } from "./models/LoginRequest.ts";
+export type { LoginResponse } from "./models/LoginResponse.ts";
+export type {
+  PingRequestConfig,
+  PingResponses,
+  PingStatus200,
+} from "./models/Ping.ts";
+export type { PingResponse } from "./models/PingResponse.ts";
+export type {
+  RegisterData,
+  RegisterRequestConfig,
+  RegisterResponses,
+  RegisterStatus201,
+  RegisterStatus409,
+  RegisterStatus422,
+} from "./models/Register.ts";
+export type { RegisterRequest } from "./models/RegisterRequest.ts";
+export type { RegisterResponse } from "./models/RegisterResponse.ts";
+export type {
+  SystemStatusRequestConfig,
+  SystemStatusResponses,
+  SystemStatusStatus200,
+} from "./models/SystemStatus.ts";
+export type { SystemStatusResponse } from "./models/SystemStatusResponse.ts";
+export type { ValidationError } from "./models/ValidationError.ts";
+export type { ValidationErrors } from "./models/ValidationErrors.ts";
---
+++ client/generated/models/index.ts
@@ -0,0 +1,35 @@
+export type { ErrorResponse } from "./ErrorResponse.ts";
+export type {
+  LoginData,
+  LoginRequestConfig,
+  LoginResponses,
+  LoginStatus200,
+  LoginStatus401,
+  LoginStatus422,
+} from "./Login.ts";
+export type { LoginRequest } from "./LoginRequest.ts";
+export type { LoginResponse } from "./LoginResponse.ts";
+export type {
+  PingRequestConfig,
+  PingResponses,
+  PingStatus200,
+} from "./Ping.ts";
+export type { PingResponse } from "./PingResponse.ts";
+export type {
+  RegisterData,
+  RegisterRequestConfig,
+  RegisterResponses,
+  RegisterStatus201,
+  RegisterStatus409,
+  RegisterStatus422,
+} from "./Register.ts";
+export type { RegisterRequest } from "./RegisterRequest.ts";
+export type { RegisterResponse } from "./RegisterResponse.ts";
+export type {
+  SystemStatusRequestConfig,
+  SystemStatusResponses,
+  SystemStatusStatus200,
+} from "./SystemStatus.ts";
+export type { SystemStatusResponse } from "./SystemStatusResponse.ts";
+export type { ValidationError } from "./ValidationError.ts";
+export type { ValidationErrors } from "./ValidationErrors.ts";
