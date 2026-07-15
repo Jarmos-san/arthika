f2e2061b20507e9f705ce73b9e88ab64b1a2404e
Somraj Saha <31373860+jarmos-san@users.noreply.github.com>
2026-07-15 17:31:47 +0530

    feat(client): replace NuxtUI with Reka-UI (#46)
    ## Description
    
    This PR provides the necessary configurations and foundational code to
    add and set up the Reka UI component library and TailwindCSS framework
    to style the components with. Included with the configurations and code
    changes are slight updates which are not directly related to the context
    of the PR.
    
    ## Modification
    
    The changes introduced with the PR are as follows:
    1. Installed and setup TailwindCSS for the project.
    2. Added an experimental `Checkbox` component (from Reka UI) and styled
    it using TailwindCSS. This is done to check if the introduced changes
    work as expected.
    3. Updated the Nuxt.js configuration (`nuxt.config.ts`) file to replace
    NuxtUI and use Reka UI as the preferred component library of choice.
    4. Updated some fixes in the `.gitignnore` and `.gitattributes` which
    were causing the CSS and TypeScript files to be not be detected
    properly.

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,11 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2026-06-23",
+  css: ["./app/assets/css/main.css"],
   future: {
     compatibilityVersion: 4,
   },
-  modules: ["@nuxt/ui"],
+  modules: ["reka-ui/nuxt"],
   ssr: false,
   typescript: {
     strict: true,
+  },
+  vite: {
+    optimizeDeps: {
+      include: ["@iconify/vue"],
+    },
+    plugins: [tailwindcss()],
   },
 });
