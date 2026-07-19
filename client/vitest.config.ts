2e60fb8e76c10dcceda3f8671903d1cea5c4798a
Somraj Saha <31373860+jarmos-san@users.noreply.github.com>
2026-07-19 20:36:27 +0530

    chore(client): set up and configure automated tests (#60)
    ## Description
    
    This PR provides an update to set up Vitest for the project's
    client-side application.
    
    ## Modification
    
    The changes and modifications introduced with the PR are as follows:
    
    1. Installed and configured Vitest for the project's client-side
    application.
    2. Updated the CI to run the automated test suite on each PR created.
    3. Added a sample test to check if Vitest was installed and running
    properly.
    4. Added a `task ...` command to run the tests for the client-side
    application.

---
+++ client/vitest.config.ts
@@ -0,0 +1,60 @@
+import { defineVitestProject } from "@nuxt/test-utils/config";
+import {
+  configDefaults,
+  coverageConfigDefaults,
+  defineConfig,
+} from "vitest/config";
+
+const MAX_FAILURES = 3;
+const MAX_RETRIES_IN_CI = 3;
+const MAX_RETRIES_IN_LOCAL = 0;
+const MAX_WORKERS = 4;
+
+export default defineConfig({
+  test: {
+    bail: process.env.CI ? MAX_FAILURES : undefined,
+    coverage: {
+      enabled: true,
+      exclude: [
+        "*.config.ts",
+        "**/app.vue",
+        "**/types/*.ts",
+        "**/pages/**/*.vue",
+        "**/layouts/**/*.vue",
+        ...coverageConfigDefaults.exclude,
+      ],
+      reporter: ["text", "json"],
+    },
+    fileParallelism: process.env.CI ? true : undefined,
+    logHeapUsage: true,
+    maxWorkers: process.env.CI ? MAX_WORKERS : "100%",
+    pool: "threads",
+    printConsoleTrace: true,
+    projects: [
+      await defineVitestProject({
+        test: {
+          environment: "nuxt",
+          include: ["tests/nuxt/**/*.test.ts"],
+          isolate: false,
+          mockReset: true,
+          name: "nuxt",
+        },
+      }),
+      {
+        test: {
+          environment: "node",
+          include: ["tests/unittests/**/*.test.ts"],
+          mockReset: true,
+          name: "unit",
+        },
+      },
+    ],
+    reporters: [
+      ...configDefaults.reporters,
+      ...(process.env.GITHUB_ACTIONS ? ["github-actions"] : []),
+    ],
+    retry: process.env.CI ? MAX_RETRIES_IN_CI : MAX_RETRIES_IN_LOCAL,
+    silent: process.env.CI ? false : "passed-only",
+    watch: false,
+  },
+});
