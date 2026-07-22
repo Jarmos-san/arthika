e77fe5bf0f4142c2ad85483f470e33a6db138796
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-07-22 18:05:50 +0530

    chore(client): fix auth middleware bug

--- client/app/middleware/auth.global.ts
+++ client/app/middleware/auth.global.ts
@@ -1,11 +1,12 @@
 import useAuthStore from "~/stores/auth";
 import getRedirectPath from "~/utils/routing";
 
+// oxlint-disable-next-line eslint/require-await
 export default defineNuxtRouteMiddleware(async (to) => {
   const { isAuthenticated } = useAuthStore();
   const redirectPath = getRedirectPath(isAuthenticated, to.path);
 
   if (redirectPath !== undefined) {
-    await navigateTo(redirectPath);
+    return navigateTo(redirectPath);
   }
 });
