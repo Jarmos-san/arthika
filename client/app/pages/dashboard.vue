606e85a48573ea2b03875f6cd1676ac034555ded
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-07-31 18:43:54 +0530

    chore: refactor the dashboard

--- client/app/pages/dashboard.vue
+++ client/app/pages/dashboard.vue
@@ -5,9 +5,8 @@
 </script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center">
-    <h1 class="text-2xl font-semibold tracking-tight text-stone-800">
-      Hello World!
-    </h1>
+  <div>
+    <AppNavbar />
+    <div class="p-6">Hello World!</div>
   </div>
 </template>
